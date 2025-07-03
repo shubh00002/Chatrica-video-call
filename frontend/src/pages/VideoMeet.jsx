52717cba192cc9ee31ad5d8e7c1dcd24a83d9e1b
shubh sharma <shubhs743@gmail.com>
2025-07-04 01:29:09 +0530

    adding enviroment.js

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -11,7 +11,7 @@ import MicOffIcon from '@mui/icons-material/MicOff'
 import ScreenShareIcon from '@mui/icons-material/ScreenShare';
 import StopScreenShareIcon from '@mui/icons-material/StopScreenShare'
 import ChatIcon from '@mui/icons-material/Chat'
-import server from '../environment.js';
+import server from "../enviroment.js";
 
 const server_url = server;
 
